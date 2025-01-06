ad767d256f44a9bdc4ef0bf164e7171d7ca77ea2
jh-benjamin <jaehwan.lee.biz@gmail.com>
2025-01-06 15:04:37 +0900

    hi

--- app/main.py
+++ app/main.py
@@ -160,14 +160,16 @@ async def analyze_receipt(file: UploadFile = File(...)):
 
         response_dict = response.to_dict()
         
-        print(response_dict)
-        print(response_dict["choices"])
+        print(response_dict["choices"][0])
+        print(response_dict["choices"][0]["message"])
 
         # OpenAI 응답에서 choices[0].message.content 추출
         raw_content = response_dict["choices"][0]["message"]["content"]
         
+        print(raw_content)
+
         # content에서 JSON 부분만 추출
-        json_start_index = raw_content.find("{")
+        json_start_index = raw_content.find("```json")
         json_data = raw_content[json_start_index:]
         parsed_data = json.loads(json_data)
         
